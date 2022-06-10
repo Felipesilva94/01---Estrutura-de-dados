99138a28368a55c2bd67ba44a59f9bbc0f52aba4
Felipesilva94 <lippepunhal@gmail.com>
2022-06-09 22:35:39 -0300

    estrutura de dados

---
+++ script.js
@@ -0,0 +1,29 @@
+// Crie uma variável que receba um nome;
+
+let nome = 'Felipe';
+
+// Crie uma variável que receba um número;
+
+let idade = 28;
+
+// Crie uma variável com o ano atual e subtraia pelo número que você guardou;
+
+let ano = 2022;
+console.log(ano -  idade)
+
+
+// Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
+console.log(ano + idade)
+
+//Mostre no console a mensagem "Estou mexendo no console.log";
+
+let codando = 'Estou mexendo no console.log';
+console.log(codando)
+
+
+// Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa verdadeiro ou falso e mostre a saída no console;
+let quartaFeira = true;
+console.log(quartaFeira)
+
+// Exiba no console o tipo de dado da váriavel quartaFeira;
+console.log(typeof quartaFeira)
